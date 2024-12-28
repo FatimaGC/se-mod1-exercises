10969330ded5f42db76a7c5170dd013c14de6474
Fatima Guzman <fguzman1230@gmail.com>
2024-12-28 11:34:15 -0700

    Find exercises

--- ruby_exercises/enumerables/exercises_1/spec/find_spec.rb
+++ ruby_exercises/enumerables/exercises_1/spec/find_spec.rb
@@ -8,35 +8,43 @@ RSpec.describe 'find test' do
     expect(found).to eq("unicorn")
   end
 
-  xit 'no waldo' do
+  it 'no waldo' do
     words = ["scarf", "sandcastle", "flag", "pretzel", "crow", "key"]
     found = words.find do |word|
-      # Your code goes here
+      word == "waldo"
     end
     expect(found).to eq(nil)
   end
 
-  xit 'found waldo' do
+  it 'found waldo' do
     words = ["noise", "dog", "fair", "house", "waldo", "bucket", "fish"]
-    # Your code goes here
+    found = words.find do |word|
+      word == "waldo"
+    end
     expect(found).to eq("waldo")
   end
 
-  xit 'no three letter words' do
+  it 'no three letter words' do
     words = ["piglet", "porridge", "bear", "blueberry"]
-    # Your code goes here
+    found = words.find do |word|
+      word.length == 3
+    end
     expect(found).to eq(nil)
   end
 
-  xit 'find 13' do
+  it 'find 13' do
     numbers = [2, 13, 19, 8, 3, 27]
-    # Your code goes here
+    found = numbers.find do |number|
+      number == 13
+    end
     expect(found).to eq(13)
   end
 
-  xit 'find first even number' do
+  it 'find first even number' do
     numbers = [3, 7, 13, 11, 10, 2, 17]
-    # Your code goes here
+    found = numbers.find do |number|
+      number.even?
+    end
     expect(found).to eq(10)
   end
 
