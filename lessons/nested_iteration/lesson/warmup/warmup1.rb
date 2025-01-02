3e61a0ecd965d0766fbd93fc7089a3765ded0020
Fatima Guzman <fguzman1230@gmail.com>
2025-01-02 11:59:03 -0700

    Complete warmup1

--- lessons/nested_iteration/lesson/warmup/warmup1.rb
+++ lessons/nested_iteration/lesson/warmup/warmup1.rb
@@ -5,9 +5,25 @@ animals = [:dog, :cat, :zebra, :quokka, :unicorn, :bear]
 # Use an enumerable to
 # 1. Return an array of animals as strings.
 
+# animals_as_strings = []
+
+# animals.each do |animal|
+#   animals_as_strings.push(animal.to_s)
+# end
+
+# p animals_as_strings
 
 
+animals_as_strings = animals.map do |animal|
+  animal.to_s
+end
 
-
+p animals_as_strings
 
 # 2. Return an array of animals with a length >= 4.
+
+shortened_animals = animals.filter do |animal|
+  animal.length <= 4
+end
+
+p shortened_animals
