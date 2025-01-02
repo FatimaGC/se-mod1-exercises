5b48e8f98a4cd493af7e1005c9f46302043f025d
Fatima Guzman <fguzman1230@gmail.com>
2025-01-02 15:29:04 -0700

    Complete exercise

--- lessons/nested_iteration/lesson/exercise/cities_lived_in.rb
+++ lessons/nested_iteration/lesson/exercise/cities_lived_in.rb
@@ -10,8 +10,15 @@ cities_lived_in = {
 # Get a unique list of all of the cities that these humans have lived in  
 # ["Philadelphia", "Fort Collins", "Seattle", "Denver", "Santa Fe", "Portland", "Lansing", "Columbus", "Austin"]
 
+#Iterate through the hash
+#Get the array for each key
+#Return all cities in a single array
 
+# all_cities = cities_lived_in.flat_map do |key, value|
+#     value
+# end
 
+# p all_cities
 
 # Problem #2: 
 # Write code that iterates through the `cities_lived_in` hash, and returns a list of  
@@ -21,8 +28,9 @@ cities_lived_in = {
 # or
 # ["Michaela", "Mike", "Alex"]
 
+# philadelphia_inhabitants = cities_lived_in.select {|key, value| value.include?("Philadelphia")}.keys
 
-
+# p philadelphia_inhabitants
 
 # Problem #3: 
 # Create a hash that has the city as a key, and the number of people that live in it as it's value: 
@@ -38,3 +46,24 @@ cities_lived_in = {
 #     "Columbus => 1,
 #     "Austin" => 1
 # }
+
+#Start with an empty hash names "cities"
+#Iterate through the hash
+#In each array, check whether each city is already in the cities hash
+    #If the city is NOT in the hash, add it to the hash as a new key
+    #If the city is already in the hash, + 1 to that city's value 
+
+cities = {}
+
+cities_lived_in.each do |key, value|
+    value.each do |city| 
+        if !cities.has_key?(city)
+            cities[city] = 1
+        else 
+            cities[city] += 1
+        end
+    end
+end
+
+
+p cities
